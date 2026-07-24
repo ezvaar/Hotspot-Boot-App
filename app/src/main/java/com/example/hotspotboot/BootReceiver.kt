173aa44204e817db17597972de3faf36c702c23f
ezvaar <tshepiso.khunou@gmail.com>
2026-07-24 22:30:47 +0200

    Update BootReceiver.kt

--- app/src/main/java/com/example/hotspotboot/BootReceiver.kt
+++ app/src/main/java/com/example/hotspotboot/BootReceiver.kt
@@ -1,1 +1,24 @@
+package com.example.hotspotboot
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val action = intent.action
+        if (action == Intent.ACTION_BOOT_COMPLETED ||
+            action == "android.intent.action.QUICKBOOT_POWERON" ||
+            action == Intent.ACTION_LOCKED_BOOT_COMPLETED ||
+            action == Intent.ACTION_MY_PACKAGE_REPLACED
+        ) {
+            val serviceIntent = Intent(context, BootService::class.java)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context.startForegroundService(serviceIntent)
+            } else {
+                context.startService(serviceIntent)
+            }
+        }
+    }
+}
